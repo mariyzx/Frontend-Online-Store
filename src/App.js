324792a9ad9ba597dcc2585dc1e94e5a4ecead03
Mariana Werneck <marinhomariana8@gmail.com>
2022-09-09 18:53:00 -0300

    Requisio 1 - Requisição à API do ML

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
+  console.log(getCategories());
+  console.log(getProductsFromCategoryAndQuery('MLB1055', 'Motorola'));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Iniciando a Online Store!
     </div>
   );
 }
