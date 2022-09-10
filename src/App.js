df8664687255864ec8d7c1ae6ece5c6820e11e31
Mariana Werneck <marinhomariana8@gmail.com>
2022-09-10 13:47:51 -0300

    Requisito 4 - Listando as categorias retornadas pela API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import './App.css';
 import { Route,
   BrowserRouter as Router, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 function App() {
-  console.log(getCategories());
-  console.log(getProductsFromCategoryAndQuery('MLB1055', 'Motorola'));
   return (
     <Router>
       <Switch>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.categories();
+  }
+
+  categories = async () => {
+    this.setState({ loading: true });
+    const cat = await getCategories();
+    this.setState({ categories: cat, loading: false });
+  };
+
   render() {
     const { history } = this.props;
+    const { loading, categories } = this.state;
     return (
       <div>
         <input type="text" />
@@ -18,8 +39,16 @@ class Home extends React.Component {
           onClick={ () => history.push('/carrinho') }
         >
           Carrinho
-
         </button>
+        <div>
+          {!loading && (
+            categories.map((cat) => (
+              <label htmlFor="category" key={ cat.id } data-testid="category">
+                <input type="radio" />
+                {cat.name}
+              </label>))
+          )}
+        </div>
       </div>
     );
   }
