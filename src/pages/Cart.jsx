5b7272426d51b61a87e239ebd46149cbfb717d6e
Mariana Werneck <marinhomariana8@gmail.com>
2022-09-10 16:37:16 -0300

    Requisito 10 - Botões para aumentar e diminuir a quantidade dos produtos no carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -15,7 +15,37 @@ class Cart extends React.Component {
 
   getProducts = () => {
     const prod = JSON.parse(localStorage.getItem('cart'));
-    this.setState({ products: prod });
+    const newCart = prod.map((a) => {
+      const qnt = localStorage.getItem(prod.id);
+      a.quantity = qnt;
+      return a;
+    });
+    this.setState({ products: newCart });
+  }
+
+  countFunc = (id, { target }) => {
+    const { products } = this.state;
+    const one = 1;
+    let qnt = Number(localStorage.getItem(id));
+    if (target.value === 'decrease' && qnt === one) {
+      return false;
+    }
+    if (target.value === 'increase') {
+      qnt += 1;
+    }
+    if (target.value === 'decrease') {
+      qnt -= 1;
+    }
+    localStorage.setItem(id, qnt);
+    const arr = products.map((item) => {
+      if (item.id === id) {
+        item.quantity = qnt;
+        return item;
+      }
+      return item;
+    });
+
+    this.setState({ products: arr });
   }
 
   render() {
@@ -30,9 +60,25 @@ class Cart extends React.Component {
             <div key={ item.id }>
               <img src={ item.thumbnail } alt={ item.title } />
               <h4 data-testid="shopping-cart-product-name">{item.title}</h4>
+              <button
+                data-testid="product-decrease-quantity"
+                type="button"
+                value="decrease"
+                onClick={ (e) => this.countFunc(item.id, e) }
+              >
+                -
+              </button>
               <p data-testid="shopping-cart-product-quantity">
                 {localStorage.getItem(item.id)}
               </p>
+              <button
+                data-testid="product-increase-quantity"
+                type="button"
+                value="increase"
+                onClick={ (e) => this.countFunc(item.id, e) }
+              >
+                +
+              </button>
             </div>
           ))}
       </div>
